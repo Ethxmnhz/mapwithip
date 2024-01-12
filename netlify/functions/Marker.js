0e0ff1fe51b66b737175a2d5afcebf8930cfefbc
Minhaz <140340472+Ethxmnhz@users.noreply.github.com>
2024-01-12 16:40:01 +0530

    Update Marker.js

--- netlify/functions/Marker.js
+++ netlify/functions/Marker.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+
+const Marker = mongoose.model('Marker', {
+  lat: Number,
+  lng: Number,
+  name: String,
+  contacts: [{ name: String, phone: String, details: String }],
+});
+
+module.exports = Marker;
 
